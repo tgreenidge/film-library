e5ae77fd2ef581e66d788f3a6b737ff5d1e49920
John Bell <Johnrbell@gmail.com>
2018-04-06 09:31:33 -0400

    modified for code challenge

--- src/FilmPoster.js
+++ src/FilmPoster.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 
 class FilmPoster extends Component {
   render() {
-    const posterUrl = 'https://image.tmdb.org/t/p/w780/' + this.props.film.poster_path
+    const posterUrl = this.props.film.poster_path
 
     return (
          <img src={posterUrl} alt="" />
