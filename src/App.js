65fdaae304977d68211cf03bd65524d58af7897d
tgreenidge <tisha.m.greenidge@gmail.com>
2018-04-13 16:56:40 -0400

    initial commit. Rename jsx files to .js files

--- src/App.js
+++ src/App.js
@@ -2,10 +2,22 @@ import React, { Component } from 'react';
 import TMDB from './TMDB';
 import FilmListing from './FilmListing';
 import FilmDetails from './FilmDetails';
+ 
 
 class App extends Component {
   // this is the container componenet for the application.
   // it should render the child components, filmDetail and filmListing
+  state = {}
+
+  
+  render() {
+    const films = TMDB.films;
+    
+    return (
+      <div/>
+    )
+    
+  }
 
 }
 
--- src/FilmDetails.js
+++ src/FilmDetails.js
@@ -2,4 +2,8 @@ import React from 'react';
 
 export default function FilmDetails(props) {
   // this is the movie data that appears on the right side (title and description)
+
+  return(
+    <div/>
+  )
 }
