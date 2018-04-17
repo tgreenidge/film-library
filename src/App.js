78f4fdf6df42664cc01574093dbfe6662ce6c43c
tgreenidge <tisha.m.greenidge@gmail.com>
2018-04-17 16:15:55 -0400

    Set up favorities file and pass into FilmListings component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import TMDB from './TMDB';
+import FAVORITES from './FAVORITES';
 import FilmListing from './FilmListing';
 import FilmDetails from './FilmDetails';
  
@@ -8,14 +9,15 @@ class App extends Component {
   // this is the container componenet for the application.
   // it should render the child components, filmDetail and filmListing
   state = {}
-
   
   render() {
+
     const films = TMDB.films;
+    const faves = FAVORITES.films;
     
     return (
       <div className="film-library">
-        <FilmListing films={films}/>
+        <FilmListing allFilms={films} faveFilms={faves}/>
         <FilmDetails films={films}/>
       </div>
     )
---
+++ src/FAVORITES.js
@@ -0,0 +1,8 @@
+const FAVORITES = {
+
+    films: [
+      
+    ],
+  };
+  
+  export default FAVORITES;
