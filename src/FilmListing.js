d06a65f08f6951cb0c0edbbfce061f51a04250d4
tgreenidge <tisha.m.greenidge@gmail.com>
2018-04-17 16:31:39 -0400

    differentiate all films from fave films and add state to component

--- src/FilmListing.js
+++ src/FilmListing.js
@@ -4,22 +4,33 @@ import FilmRow from './FilmRow.js';
 class FilmListing extends Component {
   // this is the listing area where the individual film row components will appear.
 
-  render() {
-    //modify for faves count
-    const faves = 0;
+  state = {
+    showAll: true
+  }
+  
+  handleClick(filmsToShow) {
 
-    const filmRows = this.props.films.map((film, index) => {
+  }
+
+  render() {
+
+    const allFilms = this.props.allFilms;
+    const faves = this.props.faveFilms;
+
+    const films = this.state.showAll ? allFilms : faves;
+    
+    const filmRows = films.map((film, index) => {
       return <FilmRow
           key={index}
           data={film}/>;
     });
-    
+
     return(
       <div className="film-list">
         <div className="section-title">Films</div>
         <div className="film-list-filters">
-          <div className="film-list-filter">All<span className="section-count">{filmRows.length}</span></div>
-          <div className="film-list-filter">Faves<span className="section-count">{faves}</span></div>
+          <div className="film-list-filter">All<span className="section-count">{allFilms.length}</span></div>
+          <div className="film-list-filter">Faves<span className="section-count">{faves.length}</span></div>
         </div>
         {filmRows}
       </div>
